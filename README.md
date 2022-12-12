e4faf6480616cd34fc3035a4719f61c7a80ce815
unknown <jasperbennett251@hotmail.co.uk>
2022-12-12 19:08:32 +0000

    actual commit what

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Test
 Originally a test for git because it wasnt working earlier but i fixed it so i dont know what to put here honestly  
-Ill probabaly put some of my discord bots code here or something  
+Ill probabaly put some of my discord bots code here or something   
+
+## Note:
+The note above is old and i have my old bots code listed above and am actively working on it.  
--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("sakdak,al");
+const { Client, IntentsBitField, GatewayIntentBits, Events, REST, Collection } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+const { data } = require("./commands/testping");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { token } = require("./config.json");
+
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+
+client.login(token);
+
+client.on(Events.ClientReady, () => {
+    console.warn("bot is online");
+	client.user.setStatus('online');
+	client.user.setActivity('/Help for more info')
+	client.user.setPresence('https://discord.com/oauth2/authorize?client_id=1045391223634010132&scope=bot&permissions=8')
+});
+
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
